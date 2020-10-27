2bda9f8f4b44548653cbb5eeb5529cb3b820439f
iballi2017 <iballi2017@gmail.com>
2020-10-27 04:27:24 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import React, {Component} from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
-      return <div className="App">
-          <Layout>
-              <BurgerBuilder/>
-              {/* <h1>hello world!</h1> */}
-              </Layout>
-    </div>;
+    return (
+      <div className="App">
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
